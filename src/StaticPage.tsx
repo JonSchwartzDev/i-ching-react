b3ab4b94d2f50a4b3655b5338d3fcaf2c0a21d18
jonschwartzdev <jonschwartz@outlook.com>
2021-10-23 18:47:45 -0700

    Next button on trigram pages

--- src/StaticPage.tsx
+++ src/StaticPage.tsx
@@ -1,19 +1,39 @@
-import { useParams } from "react-router";
+import { useHistory, useParams } from "react-router";
 import {
   staticPageDictionary,
   staticPageNames,
   staticPageSummaries,
+  trigrams,
 } from "./data";
 
 export default function StaticPage() {
   let { name } = useParams<{ name: string }>();
+  let history = useHistory();
+
   if (name === undefined) {
     name = "tao";
   }
+
   let index = staticPageDictionary.get(name)!;
   let imgClassName = "col-2 " + (index > 7 ? "" : "trigram ") + name;
+
+  const goToNext = () => {
+    let next = index + 1;
+    if (next === 8) {
+      next = 0;
+    }
+    let url = "/" + trigrams[next];
+    history.push(url);
+  };
+
   return (
     <>
+      {index < 8 && (
+        <div className="nextbutton" onClick={goToNext}>
+          ›
+        </div>
+      )}
+
       <div className={imgClassName}></div>
       <div className="h2 col-2">{staticPageNames[index]}</div>
       {staticPageSummaries[index]!.map((para, index) => (
